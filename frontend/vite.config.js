6c78278fb447e7e846a79fe8725a04f885692a90
Renato <renato@Renatos-MacBook-Pro.local>
2025-06-21 14:42:15 +0100

    Try to fix style

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/ 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Tells Vite to treat frontend/ as root
+  root: '.', // ensures index.html is found
 });
